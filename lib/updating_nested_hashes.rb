f21149ec42c4c2b06f236bfbee614b5bc88c0294
Kimberlyn Stoddard <kimberlynrstoddard@gmail.com>
2020-07-02 15:01:11 +0000

    Done.

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -20,10 +20,10 @@ def update_status
       }
    }
 
-  # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
-  # Code your solution here:
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
-
+return epic_tragedy
 
 
   # After your code, the following code will return the altered hash. This method must only return the modified epic_tragedy hash
@@ -59,8 +59,11 @@ def add_characters
    #   - The first hash should have the :name key set to "Prince Escalus" 
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
+epic_tragedy[:additional_characters] = [
+  {:name => "Prince Escalus"},
+  {:name => "Apothecary"}]
 
-
+return epic_tragedy
 
 
 
